4f751a17c2e224a340a84a1e484f288ae554c526
Teppo Koskinen <orlof@users.noreply.github.com>
2021-01-23 11:59:10 +0200

    recording buffer¨

--- video_recorder.py
+++ video_recorder.py
@@ -4,51 +4,97 @@ import time
 import mss
 import threading
 import os
+import queue
 
 
 class video_recorder:
     def __init__(self):
-        self.worker = None
+        self.capture_thread = None
+        self.output_thread = None
+
+        self.state = "IDLE"
+        self.buffer = queue.Queue()
+
         self.filename = None
+        self.monitor = None
+        self.width = 0
+        self.height = 0
+        self.max_frames = 0
+        self.fps = 0.0
+        self.compress = False
 
-    def start_recorder(self, filename="recording.avi", max_length=60.0, monitor=1, scale=1.0, fps=5, compress="True"):
-        if self.worker:
-            raise ValueError("Recorder busy")
+    def start_recorder(self, filename="recording.avi", max_length=10, monitor=1, scale=1.0, fps=5.0, compress="True"):
         self.filename = filename
-        self.worker = threading.Thread(target=self._loop, args=(
-            filename, float(max_length), int(monitor), 
-            float(scale), int(fps), compress.lower() == "true"
-        ))
-        self.worker.start()
+        self.fps = float(fps)
+        self.max_frames = round(self.fps * float(max_length))
+        self.compress = compress.lower() == "true"
 
-    def stop_recorder(self, cancel=False):
-        worker = self.worker
-        self.worker = None
-        worker.join()
-        if cancel:
-            os.remove(self.filename)
+        self.state = "RUNNING"
+
+        with mss.mss() as sct:
+            # Part of the screen to capture
+            self.monitor = sct.monitors[int(monitor)]
+            self.width = round(self.monitor["width"] * float(scale))
+            self.height = round(self.monitor["height"] * float(scale))
+
+        self.output_thread = threading.Thread(target=self._write_file)
+        self.capture_thread = threading.Thread(target=self._capture)
+        self.output_thread.start()
+        self.capture_thread.start()
+
+    def stop_recorder(self):
+        self.state = "STOPPING"
+        self.capture_thread.join()
+        self.output_thread.join()
 
     def cancel_recorder(self):
-        self.stop_recorder(True)
+        self.stop_recorder()
+        os.remove(self.filename)
 
     def get_monitors(self):
         with mss.mss() as sct:
             return sct.monitors
 
-    def _loop(self, filename, max_length, monitor, scale, fps, compress):
+    def _write_file(self):
+        num_frames = 0
+        prev_frame = None
+
+        fourcc = cv2.VideoWriter_fourcc(*'VP80')
+        out = cv2.VideoWriter(self.filename, fourcc, self.fps, (self.width, self.height))
+
+        while True:
+            if num_frames == self.max_frames:
+                break
+
+            ts, frame = self.buffer.get()
+            print(num_frames, ts)
+            if frame == "END":
+                break
+
+            if compress and prev_frame is not None and (prev_frame==frame).all():
+                continue
+
+            num_frames += 1
+            prev_frame = frame
+
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGRA2BGR)
+            frame = cv2.resize(frame, (width, height))
+
+            cv2.putText(frame, '{0:.2f}'.format(ts), (10, 30),  cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 0, 255), 4)
+            out.write(frame)
+
+        out.release()
+        self.state = "STOPPING"
+
+    def _capture(self):
         with mss.mss() as sct:
-            # Part of the screen to capture
-            monitor = sct.monitors[monitor]
-            width = int(monitor["width"] * scale)
-            height = int(monitor["height"] * scale)
-
-            fourcc = cv2.VideoWriter_fourcc(*'VP80')
-            out = cv2.VideoWriter(filename, fourcc, fps, (width, height))
-
-            prev_shot = None
             frame_number = 0
             start_time = time.time()
-            while self.worker and time.time() < start_time + max_length:
+
+            while True:
+                if self.state == "STOPPING":
+                    break
+
                 trigger_time = start_time + frame_number / fps
                 while time.time() < trigger_time:
                     time.sleep(0.001)
@@ -56,21 +102,10 @@ class video_recorder:
 
                 # Get raw pixels from the screen, save it to a Numpy array
                 frame = np.array(sct.grab(monitor))
-                if compress and prev_shot is not None and (prev_shot==frame).all():
-                    continue
-                
-                print('{0:.2f}'.format(time.time() - start_time))
-                prev_shot = frame
-                frame = cv2.cvtColor(frame, cv2.COLOR_BGRA2BGR)
-                frame = cv2.resize(frame, (width, height))
+                self.buffer.put_nowait((time.time() - start_time, frame))
 
-                cv2.putText(frame, '{0:.2f}'.format(time.time() - start_time),
-                           (10, 30),  cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 0, 255), 4)
-                out.write(frame)
+        self.buffer.put_nowait((time.time() - start_time, "END"))
 
-        # Clean up
-        out.release()
-        self.worker = None
 
 
 
